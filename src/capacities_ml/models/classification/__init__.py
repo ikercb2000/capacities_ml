ce918334b4e6b66dec731f4b8cf03450a29d7ec5
ikercb2000 <ikercb2000@gmail.com>
2026-08-11 00:01:11 +0200

    feat(models/classification): add choquistic classifier model and modify __init__ for imports

--- src/capacities_ml/models/classification/__init__.py
+++ src/capacities_ml/models/classification/__init__.py
@@ -1,3 +1,4 @@
-from capacities_ml.models.classification.choquet import ChoquetClassifier
+from capacities_ml.models.classification.choquistic import ChoquisticRegression
+from capacities_ml.models.classification.linear import ChoquetClassifier
 
-__all__ = ["ChoquetClassifier"]
+__all__ = ["ChoquetClassifier", "ChoquisticRegression"]
